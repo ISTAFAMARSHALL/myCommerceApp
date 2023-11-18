22be5cf7dfbec13aeac3729c1e630c792b6afff2
istafamarshall <istafa.a.marshall@gmail.com>
2023-11-18 13:00:14 -0500

    update associations in Product User and Order models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    belongs_to :orders
+    has_many :users, through: :orders
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+    has_many :orders
+    has_many :products, through: :orders
+    
 end
