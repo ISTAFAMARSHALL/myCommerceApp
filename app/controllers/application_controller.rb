927baaeb3a9ad9e6bd6e948066afd60270414425
istafamarshall <istafa.a.marshall@gmail.com>
2023-11-18 13:18:44 -0500

    update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::API
+
+    # include ActionController::Cookies
+    
+    # before_action :authorize
+    
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+
+    private
+
+    def record_not_found
+        render json: {error: "Record not found"}, status: :not_found
+    end
+
+    def record_invalid(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+    def authorize
+        @current_user = User.find_by(id: session[:user_id])
+        render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+    end
+
 end
