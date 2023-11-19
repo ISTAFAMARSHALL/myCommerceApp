af647ed5ff56101da3d649db9056d16ba259e6a7
istafamarshall <istafa.a.marshall@gmail.com>
2023-11-19 18:13:37 -0500

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect, useContext } from "react";
+import { UserContext } from "./context/user";
 
 function App() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const {currentUser, setCurrentUser} = useContext(UserContext);
+
+  useEffect(() => {
+    fetch("/me").then((response) => {
+      if (response.ok) {
+        response.json().then((user) => {
+          setCurrentUser(user);
+          setLoggedIn(true);
+        });
+      }
+    });
+  }, [setCurrentUser]);
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>SmartBuy</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {loggedIn ? (
+          <>
+            <p>Welcome {currentUser.username}</p>
+            <button onClick={() => setLoggedIn(false)}>Logout</button>
+          </>
+        ) : (
+          <>
+            <p>Please login</p>
+            <button onClick={() => setLoggedIn(true)}>Login</button>
+          </>
+        )}
       </header>
     </div>
   );
