b8cd8a225112cd413caa25f46efad7f7b6b5d1cc
istafamarshall <istafa.a.marshall@gmail.com>
2023-11-21 19:46:03 -0500

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
   resources :products
   resources :users
   resources :orders
