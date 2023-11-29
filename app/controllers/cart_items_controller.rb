55da4dd777bfb2f932e699a93e2372ce60bacb40
istafamarshall <istafa.a.marshall@gmail.com>
2023-11-29 11:14:40 -0500

    add handleUpdateQuantity function

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,28 +1,46 @@
 class CartItemsController < ApplicationController
 
-    
-    def create
-      # find user by id
-      user = @current_user
-  
-      # Create a new cart_item
-      @cart_item = user.carts.first.cart_items.create!(cart_params)
-  
-      if @cart_item.save
-        render json: @cart_item, status: :created, location: @cart_item
-      else
-        # puts @cart_item.errors.full_messages # Log errors for debugging
-        render json: @cart_item.errors, status: :unprocessable_entity
-      end
-    end
-  
-    private
-  
-    def cart_params
-        # puts params.inspect # Log received parameters for debugging
-        params.permit(:sku, :name, :salePrice, :image, :quantity, :cart_id)
+  def destroy
+    # find cart_item by id
+    cart_item = CartItem.find(params[:id])
+
+    # Delete the cart_item
+    cart_item.destroy
+  end
+
+  def update
+    # find cart_item by id
+    cart_item = CartItem.find(params[:id])
+
+    # Update the cart_item
+    cart_item.update!(cart_params)
+
+    render json: cart_item
+  end
+
+  def create
+    # find user by id
+    user = @current_user
+
+    # Create a new cart_item
+    @cart_item = user.carts.first.cart_items.create!(cart_params)
+
+    if @cart_item.save
+      render json: @cart_item, status: :created, location: @cart_item
+    else
+      # puts @cart_item.errors.full_messages # Log errors for debugging
+      render json: @cart_item.errors, status: :unprocessable_entity
     end
   end
+
+  private
+
+  def cart_params
+      # puts params.inspect # Log received parameters for debugging
+      params.permit(:sku, :name, :salePrice, :image, :quantity, :cart_id)
+  end
+
+end
 
 
 
--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,4 +1,86 @@
-import React, { useEffect, useState , useContext} from 'react';
+// import React, { useEffect, useState , useContext} from 'react';
+// import { UserContext } from '../context/user';
+
+// const Cart = () => {
+//   const [cartItems, setCartItems] = useState([]);
+//   const { currentUser, setCurrentUser } = useContext(UserContext);
+
+//   const fetchCartItems = async () => {
+//     try {
+//       const response = await fetch(`/carts/${currentUser.id}`); // Adjust the endpoint based on your API
+
+//       if (!response.ok) {
+//         throw new Error('Error fetching cart items');
+//       }
+
+//       const data = await response.json();
+//       setCartItems(data.cart_items);
+//     } catch (error) {
+//       console.error('Error fetching cart items:', error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     // Call the async function to fetch cart items when the component mounts
+//     fetchCartItems();
+//   }, []);
+
+//   // Function to calculate the total quantity of items in the cart
+//   const calculateTotalQuantity = () => {
+//     return cartItems.reduce((total, item) => total + item.quantity, 0);
+//   };
+
+//   const calculateTotal = () => {
+//     return cartItems.reduce((total, item) => total + ((item.salePrice * item.quantity )* 1), 0);
+//   };
+
+//   const handleDeleteFromCart = async (item) => {
+//     try {
+//       const response = await fetch(`/cart_items/${item.id}`, {
+//         method: 'DELETE',
+//       });
+
+//       if (!response.ok) {
+//         throw new Error('Error deleting item from cart');
+//       }
+//       // If the item was successfully deleted from the cart, we can fetch the updated cart items
+//       fetchCartItems();
+//     } catch (error) {
+//       console.error('Error deleting item from cart:', error);
+//     }
+//   }
+
+//   return (
+//     <div className="cart">
+
+//       < div className="cart-header">
+//       <h2>Your Shopping Cart</h2>
+//       <p>Items in Cart: {calculateTotalQuantity()}</p>
+//       <p>Sales Total: ${calculateTotal()}</p>
+//       </div>
+
+//       <div className="cart-items">
+//       {cartItems.map(item => (
+//         <div key={item.id} className="product-card">
+//           <img src={item.image} alt={item.name} className="product-image" />
+//           <div className="product-details">
+//             <h3 className="product-name">{item.name}</h3>
+//             <p className="product-price">Price: ${item.salePrice}</p>
+//             <p className="product-price">Quantity: {item.quantity}</p>
+//             <button className="Remove from cart" onClick={() => handleDeleteFromCart(item)}>Delete from Cart</button>
+
+//           </div>
+//         </div>
+//       ))}
+//       </div>
+
+//     </div>
+//   );
+// };
+
+// export default Cart;
+
+import React, { useEffect, useState, useContext } from 'react';
 import { UserContext } from '../context/user';
 
 const Cart = () => {
@@ -19,37 +101,88 @@ const Cart = () => {
       console.error('Error fetching cart items:', error);
     }
   };
-
+  
   useEffect(() => {
     // Call the async function to fetch cart items when the component mounts
     fetchCartItems();
-  }, [currentUser]);
+  }, []);
 
   // Function to calculate the total quantity of items in the cart
   const calculateTotalQuantity = () => {
     return cartItems.reduce((total, item) => total + item.quantity, 0);
   };
 
+  const calculateTotal = () => {
+    const total = cartItems.reduce((total, item) => total + item.salePrice * item.quantity, 0);
+    return parseFloat(total.toFixed(2));
+  };
+
+  const handleDeleteFromCart = async (item) => {
+    try {
+      const response = await fetch(`/cart_items/${item.id}`, {
+        method: 'DELETE',
+      });
+
+      if (!response.ok) {
+        throw new Error('Error deleting item from cart');
+      }
+      // If the item was successfully deleted from the cart, we can fetch the updated cart items
+      fetchCartItems();
+    } catch (error) {
+      console.error('Error deleting item from cart:', error);
+    }
+  };
+
+  const handleUpdateQuantity = async (item, newQuantity) => {
+    try {
+      const response = await fetch(`/cart_items/${item.id}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ quantity: newQuantity }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Error updating quantity');
+      }
+      // If the quantity was successfully updated, we can fetch the updated cart items
+      fetchCartItems();
+    } catch (error) {
+      console.error('Error updating quantity:', error);
+    }
+  };
+
   return (
     <div className="cart">
-
-      < div className="cart-header">
-      <h2>Your Shopping Cart</h2>
-      <p>Total Items: {calculateTotalQuantity()}</p>
+      <div className="cart-header">
+        <h2>Your Shopping Cart</h2>
+        <p>Items in Cart: {calculateTotalQuantity()}</p>
+        <p>Sales Total: ${calculateTotal()}</p>
       </div>
 
       <div className="cart-items">
-      {cartItems.map(item => (
-        <div key={item.id} className="product-card">
-          <img src={item.image} alt={item.name} className="product-image" />
-          <div className="product-details">
-            <h3 className="product-name">{item.name}</h3>
-            <p className="product-price">Quantity: {item.quantity}</p>
+        {cartItems.map((item) => (
+          <div key={item.id} className="product-card">
+            <img src={item.image} alt={item.name} className="product-image" />
+            <div className="product-details">
+              <h3 className="product-name">{item.name}</h3>
+              <p className="product-price">Price: ${item.salePrice}</p>
+              <p className="product-price">
+                Quantity:
+                <input
+                  type="number"
+                  value={item.quantity}
+                  onChange={(e) => handleUpdateQuantity(item, e.target.value)}
+                />
+              </p>
+              <button className="remove-from-cart" onClick={() => handleDeleteFromCart(item)}>
+                Delete from Cart
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       </div>
-
     </div>
   );
 };
