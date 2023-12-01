bb700c8a4b1b838f434e2573d91b2845e4ede7d6
istafamarshall <istafa.a.marshall@gmail.com>
2023-12-01 00:35:47 -0500

    update index.js and orderScreen.js

--- client/src/components/OrderScreen.js
+++ client/src/components/OrderScreen.js
@@ -1,45 +1,78 @@
-import React  from 'react';
-import { useEffect , useState } from 'react';
+import React, { useEffect, useState , useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { UserContext } from "../context/user";
 
-const OrderScreen = ({  }) => {
-
+const OrderScreen = () => {
   const [order, setOrder] = useState([]);
+  const { currentUser} = useContext(UserContext);
 
-  const { id } = useParams();// This is how we access the URL parameters
-  
-  console.log("id", id);
+  const { id } = useParams();
 
   const fetchOrder = async () => {
     try {
-      const response = await fetch(`/orders/${id}`); // Adjust the endpoint based on your API
+      const response = await fetch(`/orders/${id}`);
 
       if (!response.ok) {
-        throw new Error('Error fetching cart items');
+        throw new Error('Error fetching order');
       }
 
       const data = await response.json();
-    
-      setOrder(data);
+
+      // Check if data.order_items exists before parsing
+      const orderItemsArray = data.order_items ? JSON.parse(data.order_items) : [];
+
+      const orderItemsKeyValuePairs = orderItemsArray.map((orderItem) => {
+        const keyValuePairs = orderItem.split(',').map((pair) => {
+          const [key, value] = pair.split(':').map((str) => str.trim());
+          return { [key]: value };
+        });
+
+        return keyValuePairs.reduce((acc, pair) => ({ ...acc, ...pair }), {});
+      });
+
+      setOrder(orderItemsKeyValuePairs);
+
     } catch (error) {
-      console.error('Error fetching cart items:', error);
+      console.error('Error fetching order:', error);
     }
-  }
+  };
 
   useEffect(() => {
-    // Call the async function to fetch cart items when the component mounts
     fetchOrder();
-  }, []);
-  
+  }, [id]);
 
   console.log(order);
+
+  const orderTotal = () => {
+    const total = order.reduce((acc, item) => acc + item.salePrice * item.quantity, 0);
+    return parseFloat(total.toFixed(2));
+  };
 
   return (
     <div className="order-screen">
       <h2>Order Summary</h2>
-      {/* <p>Total Amount: ${orderTotal.toFixed(2)}</p>
-      <p>Your order has been submitted successfully!</p> */}
-      {/* You can provide additional order details here */}
+      <p></p>
+      <p>{currentUser.name} your order of {order.length} items has been submitted successfully!</p>
+
+      {/* Additional order details */}
+      <br></br>
+      <div className="order-details">
+
+
+        {/* Display details for each order item */}
+        <ul>
+          {order.map((item, index) => (
+            <li key={index}>
+              <p>Sku: {item.sku} Quantity: {item.quantity} </p>
+              <p>Name: {item.name} </p>
+              <p>Sale Price: ${item.salePrice}</p>
+              <p></p>
+            </li>
+            
+          ))}
+        </ul>
+        <h2>Total ${orderTotal()}</h2>
+      </div>
     </div>
   );
 };
