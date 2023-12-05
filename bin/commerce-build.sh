11865a3468e70edc1e517c0e0d45af735dbe88af
istafamarshall <istafa.a.marshall@gmail.com>
2023-12-05 12:24:12 -0500

    update commerce.build

--- bin/commerce-build.sh
+++ bin/commerce-build.sh
@@ -8,13 +8,9 @@ npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 bundle install
-bundle exec rake db:drop
+# bundle exec rake db:drop
 bundle exec rake db:create
 
 # Add build commands for back end
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:migrate
-
-
-
-# bundle exec rake db:migrate
-bundle exec rake db:seed
+bundle exec rake db:migrate
+bundle exec rake db:reset
